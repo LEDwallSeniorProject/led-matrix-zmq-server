2e863c46cf3cd84ad6c448f7933635156c326546
Alex Miller <alexiscool2003@sbcglobal.net>
2024-11-12 15:06:20 -0500

    remove show-hz

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -10,8 +10,8 @@ sudo ../build/led-matrix-zmq-server \
 	--gpio-slowdown 3 \
 	--frame-endpoint "tcp://*:55000" \
 	--control-endpoint "tcp://*:55001" \
-	--show-hz \
 	--pwm-bits 8 \
 	--limit-hz 120 \
 	--max-brightness 100 \
 	--busy-waiting 
+	#--show-hz \
